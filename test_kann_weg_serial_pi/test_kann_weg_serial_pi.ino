4ac056501594263cbcde0276a64bd3515735cbcc
berlinEagles <54119011+berlinEagles@users.noreply.github.com>
2019-08-14 21:21:22 +0200

    Add files via upload

---
+++ test_kann_weg_serial_pi/test_kann_weg_serial_pi.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+Serial.print("Ich bin...");
+delay(1);
+Serial.println("der Boss!");
+delay(1);
+Serial.println("Hab keine Ahnung, was das b und /r/n am Ende soll?!");
+delay(1);
+}
